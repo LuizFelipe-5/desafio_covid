07ccecd5bddbc02817a361123eb0b0e0f3d0966d
Luiz Felipe Montuani <luiz.silva16@estudante.ufla.br>
2021-04-12 22:58:54 -0300

    create pages, splashScreen and app icon

---
+++ lib/pages/continents.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+import 'splash.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Splash Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Splash Screen'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return _splashScreen();
+  }
+}
+
+Widget _splashScreen() {
+  return Stack(
+    children: [
+      SplashScreen(
+        seconds: 10,
+        backgroundColor: Colors.white,
+        navigateAfterSeconds: HomeScreen(),
+        loaderColor: Colors.transparent,
+      ),
+      Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("./assets/logo.png"),
+            fit: BoxFit.none,
+          ),
+        ),
+      )
+    ],
+  );
+}
